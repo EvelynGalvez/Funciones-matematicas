900e598b530a2b29ff991673b2cc1baeb233028a
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-26 00:04:28 -0400

    js listo, html vacío

---
+++ app.js
@@ -0,0 +1,10 @@
+function multiply() {
+    let number1 = 9824;
+    let number2 = 777; 
+    let number3 = 36325;
+    function add(){
+        return number1+number2; 
+    } 
+    return number3 * add();
+}
+console.log(multiply());
