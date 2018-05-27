5632dd718c6d99c0059aff5bb6a65c17629406a3
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-26 22:49:11 -0400

    cambio a ES6 y muestra en html

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-function multiply() {
-    let number1 = 9824;
-    let number2 = 777; 
-    let number3 = 36325;
-    function add(){
-        return number1+number2; 
-    } 
-    return number3 * add();
-}
-console.log(multiply());
+const add = (num1, num2) => num1 + num2;
+const multiply = (num3, num4) => num3*num4;
+
+document.write(multiply(36325,(add(9824,777))));
